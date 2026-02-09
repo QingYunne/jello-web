f1d399d680ca589c24bc4b8ec94dffbe29ef361f
Thanh Van Nguyen <qingyunruan1605@gmail.com>
2026-02-09 12:03:56 +0000

    refactor relative and absolute path for import dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
-import App from './App.jsx'
-import theme from './theme.js'
+import App from '~/App.jsx'
+import theme from '~/theme.js'
 // import { ThemeProvider } from '@mui/material/styles'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
