b6020d4170e7c4c937a875c98c2930bfc43df321
Nguyễn Thị Thanh Vân <qingyunruan1605@gmail.com>
2026-06-11 16:40:24 +0700

    Boards (#29)
    * feat: get boards for user
    
    * feat: create board and authenticate when call API for boards
    
    * feat: update column's title
    
    * feat: active card modal
    
    * feat: update card description, cover, query board owners and board members
    
    * feat: card comment and invitation user to board
    
    * feat: add member to board and notifications
    
    * fix: render avatar when image is missing
    
    * refactor: use environment variables for API configuration

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:8017'
-export const API_VERSION = 'v1'
+export const API_ROOT = import.meta.env.VITE_API_ROOT ?? 'http://localhost:8017'
+export const API_VERSION = import.meta.env.VITE_API_VERSION ?? 'v1'
 export const INTERCEPTOR_LOADING_CLASSNAME = 'interceptor-loading'
 export const API_BASE_URL = `${API_ROOT}/${API_VERSION}`
 
